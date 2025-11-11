c02ab7549e5451c9bb0779c26bb400899c6492cc
4Sinan <sinan95han@gmail.com>
2025-11-11 22:01:25 +0300

    Add Flask app to calculate average of two numbers

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/ortalama", methods=['GET'])
+def ortalama_hesapla():
+    
+    sayi1_str = request.args.get('sayi1')
+    sayi2_str = request.args.get('sayi2')
+
+    if sayi1_str is None or sayi2_str is None:
+        return jsonify({"hata": "Lütfen 'sayi1' ve 'sayi2' parametrelerini girin."}), 400
+
+    try:
+        sayi1 = float(sayi1_str)
+        sayi2 = float(sayi2_str)
+    except ValueError:
+        return jsonify({"hata": "Parametreler sayısal bir değer olmalıdır."}), 400
+
+    sonuc = (sayi1 + sayi2) / 2
+
+    return jsonify({"ortalama_sonucu": sonuc}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
